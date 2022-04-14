902c542666da4bfcd39d31e23a50eae7af2e4181
rodionbgd <rodionbgd@mail.ru>
2022-04-14 15:14:57 +0300

    ci: Правка пути для деплоя на gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue3-todo-app/" : ".",
+  publicPath: process.env.NODE_ENV === "production" ? "/validation-recurse/" : ".",
 };
